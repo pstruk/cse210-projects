5bbcd59a6efd2722bcdfac7a4e49a34641cc1361
pstruk <str17021@byui.edu>
2024-11-04 16:55:16 -0500

    Finished Prep2 Activity.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Authentication.ExtendedProtection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userPercentageInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userPercentageInput);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A"; 
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B"; 
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed your course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry that you didn't pass. You'll do better next time.");
+        }
+
     }
 }
