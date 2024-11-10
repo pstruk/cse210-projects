795533a7cba1e44b56da31eae5a6b1c4badffc17
pstruk <str17021@byui.edu>
2024-11-09 19:01:41 -0500

    Completed Prep5 Activity

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int numSquared = SquareNumber(favNumber);
+        DisplayResult(userName, numSquared);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        string userName = "";
+        Console.Write("Please enter your name: ");
+        userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        int favNumber = 0;
+        Console.Write("Please enter your favorite number: ");
+        favNumber = int.Parse(Console.ReadLine());
+        return favNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string userName, int numSquared)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {numSquared}");
     }
 }
