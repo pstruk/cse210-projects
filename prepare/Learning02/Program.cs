ba6267e00e8e6b89ae82e018a1baa0bad231e8fa
pstruk <str17021@byui.edu>
2024-11-14 18:45:34 -0500

    Learning 02 Abstraction Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        //Step 4-Job Test: Console.WriteLine($"{job1._company}");
+        //Step 5-call Display Method: job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Apple";
+        job2._startYear = 2016;
+        job2._endYear = 2019;
+
+        //Step 4-Job Test: Console.WriteLine($"{job2._company}");
+        //Step 5-call Display Method: job2.DisplayJobDetails();
+
+        Resume resume = new Resume();
+        resume._name = "Timothy Smith";
+        
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        //Step 7-Resume Test: Console.WriteLine($"{resume._jobs[0]._jobTitle}");
+        resume.DisplayResume();
     }
 }
