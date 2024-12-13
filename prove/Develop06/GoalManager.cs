df453c833cc69cd16da22588014cd4bb6b2f2c86
pstruk <str17021@byui.edu>
2024-12-12 21:32:14 -0500

    W06-Polymorphism Design Activity- stubs

---
+++ prove/Develop06/GoalManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager()
+    {
+       
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo()
+    {
+
+    }
+
+    public void ListGoalNames()
+    {
+
+    }
+
+    public void ListGoalDetails()
+    {
+
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public void SaveGoals()
+    {
+
+    }
+
+    public void LoadGoals()
+    {
+        
+    }
+
+}
---
+++ prove/Develop06/SimpleGoal.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, string points) 
+               : base(name, description, points)
+    {
+
+    }
+
+    public override void RecordEvent()
+    {
+
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+
+}
